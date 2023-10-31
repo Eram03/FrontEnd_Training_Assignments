708b0655bd5aa456532387dcc704833811f6a361
Eram03 <147516230+Eram03@users.noreply.github.com>
2023-10-31 22:49:15 +0530

    Add files via upload

---
+++ Day 11/assign 2 6.js
@@ -0,0 +1,9 @@
+        var empid ="65788";
+        var ename ="sam";
+        
+        var job ="AA";
+        var salary = 56000;
+        
+        var str = `Empno :${empid} 12645,  Ename  :${ename}  Scott,  Job :${job} Manager, Salary :${salary} 45000`;
+
+        console.log(str);
